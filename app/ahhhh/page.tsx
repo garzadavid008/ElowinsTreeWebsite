ef223d600b604526715bdae65b5f9d6ec99c7be8
jojazpeitia <jojazpeitia@gmail.com>
2025-08-24 15:45:04 -0500

    setup panoramic background demo with react-three-fiber and drei

--- app/ahhhh/page.tsx
+++ app/ahhhh/page.tsx
@@ -1,8 +1,52 @@
+'use client';
+import { Canvas, useThree } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { TextureLoader } from "three";
+import * as THREE from 'three'
+import styles from "./page.module.css";
+
+function Background() {
+  const { scene } = useThree();
+  let bgMesh;
+
+  const loader = new TextureLoader();
+  const texture = loader.load(
+      "https://threejsfundamentals.org/threejs/resources/images/equirectangularmaps/tears_of_steel_bridge_2k.jpg"
+  );
+  texture.magFilter = THREE.LinearFilter; // what the flip is this?
+  texture.minFilter = THREE.LinearFilter; // what the flip is this?
+
+  const shader = THREE.ShaderLib.equirect;
+
+  const material = new THREE.ShaderMaterial({
+      fragmentShader: shader.fragmentShader,
+      vertexShader: shader.vertexShader,
+      uniforms: shader.uniforms,
+      depthWrite: false,
+      side: THREE.BackSide
+  });
+
+  material.uniforms.tEquirect.value = texture;
+  const plane = new THREE.SphereGeometry(20, 20, 20);
+
+  bgMesh = new THREE.Mesh(plane, material);
+  scene.add(bgMesh);
+
+  // scene.background = texture;
+  return null;
+}
+
 export default function TeaserPage() {
-    return (
+  return (
+    <div className={styles.page}>
       <div>
-        <h1>Teaser Page</h1>
-        <p>ahhhhh</p>
+        <Canvas camera={{ position: [0, 0, 40], fov: 120}}>
+          <OrbitControls/>
+          <Background />
+          <ambientLight intensity={0.5}/>
+          <spotLight intensity={400} position={[0, 5, 10]} angle={0.3} />
+        </Canvas>
       </div>
-    );
+    </div>
+  );
   }
